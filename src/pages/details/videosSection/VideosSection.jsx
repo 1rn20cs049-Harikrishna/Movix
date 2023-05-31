97265a7f43ef4b0c9bbd2771c82e7941af16f707
1rn20cs049-Harikrishna <harikrishna92364@gmail.com>
2023-05-31 21:20:56 +0530

    VideoSection updated

--- src/pages/details/videosSection/VideosSection.jsx
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -4,7 +4,7 @@ import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import {PlayIcon} from '../PlayIcon'
-import {VideoPopup} from "../../../components/videoPopUp/VideoPopUp";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 import Img from "../../../components/lazyLoadImage/Img";
 
 
